e8db26b33aa88a7e5985486bc5a2ee0be4c4c04c
junefish1414 <acc101130@gm.ntcu.edu.tw>
2021-12-02 14:43:49 +0800

    first_check

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login></Login>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from './Login.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Login
   },
   created () {
     console.log(process.env.VUE_APP_API, process.env.VUE_APP_PATH)
